a17c06e72e8eb50b6c0a99ad00a41a625f7f1b1b
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-05-26 21:45:22 +0300

    create authorization server config

---
+++ src/main/java/com/captain/authorizationserver/config/AuthorizationServerConfiguration.java
@@ -0,0 +1,27 @@
+package com.captain.authorizationserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AuthorizationServerConfiguration {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        return http.userDetailsService(userDetailsService)
+                .formLogin(Customizer.withDefaults()).build();
+    }
+}
