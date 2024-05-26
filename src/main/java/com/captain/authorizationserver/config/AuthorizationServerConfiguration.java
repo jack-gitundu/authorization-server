f4e7677eeedd855da3c6176dc48d8cef4f27da47
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-05-26 21:55:56 +0300

    add JWKSet configuration

--- src/main/java/com/captain/authorizationserver/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/captain/authorizationserver/config/AuthorizationServerConfiguration.java
@@ -1,6 +1,9 @@
 package com.captain.authorizationserver.config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.PasswordLookup;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -11,11 +14,27 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
 @Configuration
 public class AuthorizationServerConfiguration {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Value("${keyFile}")
+    private String keyFile;
+
+    @Value("${password}")
+    private String password;
+
+    @Value("${alias}")
+    private String alias;
 
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -23,5 +42,18 @@ public class AuthorizationServerConfiguration {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
         return http.userDetailsService(userDetailsService)
                 .formLogin(Customizer.withDefaults()).build();
+    }
+
+    private JWKSet buildJWKSet() throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException {
+        KeyStore keyStore = KeyStore.getInstance("pkcs12");
+        InputStream resourceAsStream = this.getClass().getResourceAsStream(keyFile);
+        keyStore.load(resourceAsStream, alias.toCharArray());
+
+        return JWKSet.load(keyStore, new PasswordLookup() {
+            @Override
+            public char[] lookupPassword(String s) {
+                return password.toCharArray();
+            }
+        });
     }
 }
