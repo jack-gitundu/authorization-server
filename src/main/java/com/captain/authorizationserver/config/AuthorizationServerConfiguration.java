7f46e848251de8af51af449f274f4a5ef6aa5c35
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-05-27 16:13:43 +0300

    configure token customizer to add roles in the token

--- src/main/java/com/captain/authorizationserver/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/captain/authorizationserver/config/AuthorizationServerConfiguration.java
@@ -12,16 +12,22 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.io.IOException;
@@ -30,9 +36,14 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
+import java.time.Duration;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Configuration
 public class AuthorizationServerConfiguration {
+
+    private static final String ROLES_CLAIM = "roles";
 
     @Autowired
     private UserDetailsService userDetailsService;
@@ -71,14 +82,14 @@ public class AuthorizationServerConfiguration {
         return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
     }
 
-    private JWKSet buildJWKSet() throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException {
+    private JWKSet buildJWKSet() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
         KeyStore keyStore = KeyStore.getInstance("pkcs12");
         try (InputStream fis = this.getClass().getClassLoader().getResourceAsStream(keyFile);) {
             keyStore.load(fis, alias.toCharArray());
-
             return JWKSet.load(keyStore, new PasswordLookup() {
+
                 @Override
-                public char[] lookupPassword(String s) {
+                public char[] lookupPassword(String name) {
                     return password.toCharArray();
                 }
             });
@@ -94,13 +105,31 @@ public class AuthorizationServerConfiguration {
     public RegisteredClientRepository registeredClientRepository() {
         RegisteredClient registeredClient = RegisteredClient.withId("couponservice")
                 .clientId("couponclientapp")
-                .clientSecret(passwordEncoder.encode("9999"))
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+                .clientSecret(passwordEncoder.encode("9999")).clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 .redirectUri("https://oidcdebugger.com/debug")
-                .scope("read")
-                .scope("write").build();
+                .scope("read").scope("write")
+                .tokenSettings(tokenSettings())
+                .build();
         return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+    @Bean
+    public TokenSettings tokenSettings() {
+        return TokenSettings.builder()
+                .accessTokenTimeToLive(Duration.ofMinutes(30l)).build();
+    }
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> jwtCustomizer() {
+        return context -> {
+            if (context.getTokenType().equals(OAuth2TokenType.ACCESS_TOKEN)) {
+                Authentication principal = context.getPrincipal();
+                Set<String> authorities = principal.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toSet());
+                context.getClaims().claim(ROLES_CLAIM, authorities);
+            }
+        };
     }
 }
